18df032b9d9e3886a5330978cf57e6adcd54544c
wangjun <16601115023@163.com>
2020-07-16 07:17:38 +0800

    feature add  添加新功能

--- springboot-swagger-demo1/src/main/java/com/itguang/springbootswaggerdemo1/web/UserController.java
+++ springboot-swagger-demo1/src/main/java/com/itguang/springbootswaggerdemo1/web/UserController.java
@@ -13,6 +13,7 @@ public class UserController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
+        System.out.println("hello");
         return "hello";
     }
 
